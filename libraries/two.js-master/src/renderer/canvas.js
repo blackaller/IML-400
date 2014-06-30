a00c658d8143a18ba242d149a2857df502e1981b
System Administrator <root@black-iMac.local>
2014-06-29 22:47:53 -0700

    adding two.js

---
+++ libraries/two.js-master/src/renderer/canvas.js
@@ -0,0 +1,271 @@
+(function() {
+
+  /**
+   * Constants
+   */
+  var mod = Two.Utils.mod;
+  var getRatio = Two.Utils.getRatio;
+
+  var canvas = {
+
+    group: {
+
+      renderChild: function(child) {
+        canvas[child._renderer.type].render.call(child, this);
+      },
+
+      render: function(ctx) {
+
+        // TODO: Add a check here to only invoke _update if need be.
+        this._update();
+
+        var matrix = this._matrix.elements;
+
+        ctx.save();
+        ctx.transform(
+          matrix[0], matrix[3], matrix[1], matrix[4], matrix[2], matrix[5]);
+
+        _.each(this.children, canvas.group.renderChild, ctx);
+
+        ctx.restore();
+
+        return this.flagReset();
+
+      }
+
+    },
+
+    polygon: {
+
+      render: function(ctx) {
+        var matrix, stroke, linewidth, fill, opacity, visible, cap, join, miter,
+            closed, commands, length, last, next, prev, a, c, d, ux, uy, vx, vy,
+            ar, bl, br, cl, x, y;
+
+        // TODO: Add a check here to only invoke _update if need be.
+        this._update();
+
+        matrix = this._matrix.elements;
+        stroke = this.stroke;
+        linewidth = this.linewidth;
+        fill = this.fill;
+        opacity = this.opacity;
+        visible = this.visible;
+        cap = this.cap;
+        join = this.join;
+        miter = this.miter;
+        closed = this.closed;
+        commands = this._vertices; // Commands
+        length = commands.length;
+        last = length - 1;
+
+        if (!visible) {
+          return this;
+        }
+
+        // Transform
+
+        ctx.save();
+
+        if (matrix) {
+          ctx.transform(
+            matrix[0], matrix[3], matrix[1], matrix[4], matrix[2], matrix[5]);
+        }
+
+        // Styles
+
+        if (fill) {
+          ctx.fillStyle = fill;
+        }
+        if (stroke) {
+          ctx.strokeStyle = stroke;
+        }
+        if (linewidth) {
+          ctx.lineWidth = linewidth;
+        }
+        if (miter) {
+          ctx.miterLimit = miter;
+        }
+        if (join) {
+          ctx.lineJoin = join;
+        }
+        if (cap) {
+          ctx.lineCap = cap;
+        }
+        if (_.isNumber(opacity)) {
+          ctx.globalAlpha = opacity;
+        }
+
+        ctx.beginPath();
+        commands.forEach(function(b, i) {
+
+          x = b.x.toFixed(3);
+          y = b.y.toFixed(3);
+
+          switch (b._command) {
+
+            case Two.Commands.close:
+              ctx.closePath();
+              break;
+
+            case Two.Commands.curve:
+
+              prev = closed ? mod(i - 1, length) : Math.max(i - 1, 0);
+              next = closed ? mod(i + 1, length) : Math.min(i + 1, last);
+
+              a = commands[prev];
+              c = commands[next];
+              ar = (a.controls && a.controls.right) || a;
+              bl = (b.controls && b.controls.left) || b;
+
+              if (a._relative) {
+                vx = (ar.x + a.x).toFixed(3);
+                vy = (ar.y + a.y).toFixed(3);
+              } else {
+                vx = ar.x.toFixed(3);
+                vy = ar.y.toFixed(3);
+              }
+
+              if (b._relative) {
+                ux = (bl.x + b.x).toFixed(3);
+                uy = (bl.y + b.y).toFixed(3);
+              } else {
+                ux = bl.x.toFixed(3);
+                uy = bl.y.toFixed(3);
+              }
+
+              ctx.bezierCurveTo(vx, vy, ux, uy, x, y);
+
+              if (i >= last && closed) {
+
+                c = d;
+
+                br = (b.controls && b.controls.right) || b;
+                cl = (c.controls && c.controls.left) || c;
+
+                if (b._relative) {
+                  vx = (br.x + b.x).toFixed(3);
+                  vy = (br.y + b.y).toFixed(3);
+                } else {
+                  vx = br.x.toFixed(3);
+                  vy = br.y.toFixed(3);
+                }
+
+                if (c._relative) {
+                  ux = (cl.x + c.x).toFixed(3);
+                  uy = (cl.y + c.y).toFixed(3);
+                } else {
+                  ux = cl.x.toFixed(3);
+                  uy = cl.y.toFixed(3);
+                }
+
+                x = c.x.toFixed(3);
+                y = c.y.toFixed(3);
+
+                ctx.bezierCurveTo(vx, vy, ux, uy, x, y);
+
+              }
+
+              break;
+
+            case Two.Commands.line:
+              ctx.lineTo(x, y);
+              break;
+
+            case Two.Commands.move:
+              d = b;
+              ctx.moveTo(x, y);
+              break;
+
+          }
+
+        });
+
+        // Loose ends
+
+        if (closed) {
+          ctx.closePath();
+        }
+
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.restore();
+
+        return this.flagReset();
+
+      }
+
+    }
+
+  };
+
+  var Renderer = Two[Two.Types.canvas] = function(params) {
+
+    this.domElement = params.domElement || document.createElement('canvas');
+    this.ctx = this.domElement.getContext('2d');
+    this.overdraw = params.overdraw || false;
+
+    // Everything drawn on the canvas needs to be added to the scene.
+    this.scene = new Two.Group();
+    this.scene.parent = this;
+
+  };
+
+  _.extend(Renderer, {
+
+    Utils: canvas
+
+  });
+
+  _.extend(Renderer.prototype, Backbone.Events, {
+
+    setSize: function(width, height, ratio) {
+
+      this.width = width;
+      this.height = height;
+
+      this.ratio = _.isUndefined(ratio) ? getRatio(this.ctx) : ratio;
+
+      this.domElement.width = width * this.ratio;
+      this.domElement.height = height * this.ratio;
+
+      _.extend(this.domElement.style, {
+        width: width + 'px',
+        height: height + 'px'
+      });
+
+      return this;
+
+    },
+
+    render: function() {
+
+      var isOne = this.ratio === 1;
+
+      if (!isOne) {
+        this.ctx.save();
+        this.ctx.scale(this.ratio, this.ratio);
+      }
+
+      if (!this.overdraw) {
+        this.ctx.clearRect(0, 0, this.width, this.height);
+      }
+
+      canvas.group.render.call(this.scene, this.ctx);
+
+      if (!isOne) {
+        this.ctx.restore();
+      }
+
+      return this;
+
+    }
+
+  });
+
+  function resetTransform(ctx) {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+  }
+
+})();
