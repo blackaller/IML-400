6670b7d22e25613c23aa531d52906d8debda1cc1
blackaller <blackaller@me.com>
2015-04-07 00:07:45 -0700

    tweak

--- apis/instagram/insta_script.js
+++ apis/instagram/insta_script.js
@@ -7,9 +7,9 @@ $.ajax({
         for (var i = 0; i < 18; i++) {
             //resolution can be "standard" or "low"
             //$("#container").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.standard_resolution.url + "'></img></a>");
-            //$(#container").append( data.data[i].user.full_name);
+            //$("#container").append( data.data[i].user.full_name);
             //$("#container").append( data.data[i].filter);
-            $("#container").append("<li><img src='" + data.data[i].user.profile_picture + "' class='user' />"
+            $("#container").append("<li><span>"+ data.data[i].user.username + " — " + data.data[i].filter + "</span> <img src='" + data.data[i].user.profile_picture + "' class='user' />"
                                     + "<a target='_blank' href='" + data.data[i].link
                                     + "' class='picture''><img src='" + data.data[i].images.low_resolution.url + "' /></a></li>"); 
         }
