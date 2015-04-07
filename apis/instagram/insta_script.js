11a19d04ffc14d95e8f1be4947d3fcf9142a9a27
blackaller <blackaller@me.com>
2015-04-06 23:44:32 -0700

    instagram api v1 style

--- apis/instagram/insta_script.js
+++ apis/instagram/insta_script.js
@@ -5,7 +5,13 @@ $.ajax({
     url: "https://api.instagram.com/v1/tags/wattstowers/media/recent?access_token=806401368.5aa13be.4a08df065cbb41469c9cc20041432d3b",
     success: function(data) {
         for (var i = 0; i < 18; i++) {
-            $(".hashtag").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></li>");
+            //resolution can be "standard" or "low"
+            //$("#container").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.standard_resolution.url + "'></img></a>");
+            //$(#container").append( data.data[i].user.full_name);
+            //$("#container").append( data.data[i].filter);
+            $("#container").append("<li><img src='" + data.data[i].user.profile_picture + "' class='user' />"
+                                    + "<a target='_blank' href='" + data.data[i].link
+                                    + " class='picture''><img src='" + data.data[i].images.low_resolution.url + "' /></a></li>"); 
         }
     }
 });
